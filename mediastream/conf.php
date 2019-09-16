2ae9e6443408c3a345a2c77177287e8908650d98
Jérémy CHABERNAUD <djerfy@gmail.com>
2019-09-16 19:52:08 +0200

    Update vars

--- mediastream/conf.php
+++ mediastream/conf.php
@@ -1,9 +1,3 @@
 <?php
-
-// path on domain where a symlink to view.php can be found
-// change only if you use web AUTH
-// example: http://mydomain.com/stream/view.php
-$streampath = '<RUTORRENT_URL>/stream/view.php'; 
-
-
+    $streampath = '<BASEURL>/stream/view.php'; 
 ?>
